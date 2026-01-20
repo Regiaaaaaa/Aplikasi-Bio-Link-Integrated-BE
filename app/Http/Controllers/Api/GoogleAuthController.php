668426871dcb38340bf50582c69ffbeccc8e7d44
Regiaaaaaa <gheizarafashaquille@gmail.com>
2026-01-20 14:33:05 +0700

    fix: missmatch url

--- app/Http/Controllers/Api/GoogleAuthController.php
+++ app/Http/Controllers/Api/GoogleAuthController.php
@@ -37,18 +37,18 @@ class GoogleAuthController extends Controller
             // Check User Banned
             if (!$user->is_active) {
                 return redirect(
-                    "http://localhost:8000/login?error=banned&message=" .
+                    "http://localhost:5173/login?error=banned&message=" .
                     urlencode($user->ban_message ?? 'Akun anda dibanned')
                 );
             }
 
-            // Create Token
+            // Create Token 
             $token = $user->createToken('auth_token')->plainTextToken;
 
-            return redirect("http://localhost:8000/google/callback?token={$token}");
+            return redirect("http://localhost:5173/google/callback?token={$token}");
 
         } catch (\Exception $e) {
-            return redirect("http://localhost:8000/login?error=google_failed");
+            return redirect("http://localhost:5173/login?error=google_failed");
         }
     }
 
